149a5d8ab3f98912bd6cb06fa4e22f506e9b6ad3
Viktor Dirin <vdirin@gmail.com>
2024-01-31 15:16:19 +0400

    Adaptation done

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function () {
     $('#dg-container').gallery();
 });
+
+$('.burger__btn').on('click', function () {
+    $('.burger__btn').toggleClass('burger__btn--active');
+    $('.header__menu-list').toggleClass('header__menu-list--active');
+})
